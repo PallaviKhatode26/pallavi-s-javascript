f7738e5508e21a4f5e6f6ac8241e679227f835f0
PallaviKhatode26 <110760492+PallaviKhatode26@users.noreply.github.com>
2022-08-30 20:22:30 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,68 @@
+//Q1.check triangle is equilateral,isoscelene,scelene
+/*let side1 = 3;
+let side2 = 1;
+let side3 = 5;
+
+ let valid = ((side1+side2)>side3)&&((side2+side3)>side1)&&((side3+side1)>side2);
+
+if (valid) {
+  console.log("it is a valid Triangle");
+}else{
+  console.log("not a valid Triangle")
+}
+
+if(side1==side2&&side2==side3){
+  console.log("equilateral Triangle");
+}else if(side1==side2||side2==side3||side3==side1){
+  console.log("isosceles triangle")
+}else{
+  console.log("scalene triangle");
+}
+*/
+
+//Q2.switch case student(marks)
+/*
+var mark=95;
+switch (true){
+  case (mark>90 && mark<=100):
+  console.log("E grade");
+  break;
+ case (mark>80 && mark<=90):
+  console.log("A grade");
+  break;
+ case (mark>70 && mark<=80):
+  console.log("B grade");
+  break;
+ case (mark>60 && mark<=70):
+  console.log("C grade");
+  break;
+ case (mark>50 && mark<=60):
+  console.log("D grade");
+  break;
+case (mark>40 && mark<=50):
+  console.log("E grade");
+  break;
+ case (mark>=35 && mark<=40):
+  console.log("pass grade");
+  break;
+case (mark>1 && mark<35):
+  console.log("fail grade");
+  break;
+default:
+  console.log("invalid marks")
+  break;
+}
+*/
+
+//Q3.sum of multiplies of 3 and 5 
+/*
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
+*/
